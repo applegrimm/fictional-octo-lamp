f0442fe6bde7e77e2763c8a1d553f6d58d3ae15d
applegrimm <applegrimm@appleivy.co.jp>
2025-10-09 12:52:19 +0900

    Update stripe-config.js

--- stripe-config.js
+++ stripe-config.js
@@ -291,7 +291,7 @@ async function createCheckoutSession(checkoutData) {
       const encodedData = btoa(encodeURIComponent(jsonString));
       
       // GAS WebアプリURL（index.htmlと同じ）
-      const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwgeG189yH0YGt6gpqpYHoclCnZe4cbo8jARRaHCqjgxpiD_XW47taPqNFlQYDhfaYaCg/exec';
+      const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz3KjsOwCNdqB3BNNhlJ4xGni7OV1sjlpjVkCuDFWkylORHGwJWl5LU24ML2q6VpiJp/exec';
       
       // JSONPリクエストURL作成
       const jsonpUrl = `${GAS_WEB_APP_URL}?action=createCheckoutSession&data=${encodedData}&callback=${callbackName}&_t=${Date.now()}`;
@@ -422,7 +422,7 @@ async function createInvoice(invoiceData) {
 
       const jsonString = JSON.stringify(invoiceData);
       const encodedData = btoa(encodeURIComponent(jsonString));
-      const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwgeG189yH0YGt6gpqpYHoclCnZe4cbo8jARRaHCqjgxpiD_XW47taPqNFlQYDhfaYaCg/exec';
+      const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz3KjsOwCNdqB3BNNhlJ4xGni7OV1sjlpjVkCuDFWkylORHGwJWl5LU24ML2q6VpiJp/exec';
       const jsonpUrl = `${GAS_WEB_APP_URL}?action=createInvoice&data=${encodedData}&callback=${callbackName}&_t=${Date.now()}`;
 
       const script = document.createElement('script');
